e606162ad3ba5cd76d365f9def2b5d5eba75cd9f
Alejandro Chanquetti <99174520+luischanquettih@users.noreply.github.com>
2023-11-21 22:12:44 -0500

    Update informacion.py

--- informacion.py
+++ informacion.py
@@ -1,8 +1,8 @@
 def username():
-    return 'maiotperu'
+    return 'username_de_mongodb'
 
 def password():
-    return '1WkWm670SKdc2dDG'
+    return 'clave_de_mongodb'
 
 def mongo_uri():
     return 'mongodb+srv://{}:{}@cluster.vpw698x.mongodb.net/?retryWrites=true&w=majority'.format(username(),password())
